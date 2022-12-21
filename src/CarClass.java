af441b63b16f0e3ff2a856207823471b910b8604
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-21 08:38:05 -0500

    the class code is ready for car project

---
+++ src/CarClass.java
@@ -0,0 +1,6 @@
+public class CarClass {
+    int doors;
+    int engine;
+    String model;
+    String color;
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        CarClass car= new CarClass();
+        car.color="red";
+        car.doors=4;
+        car.engine=5677;
+        car.model="XXL";
+    }
+}
